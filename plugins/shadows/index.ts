313a325bfae46ec3e340b0e8a45f5242429a0bf6
ajaysaagar-dev <ajaysaagar.dev@gmail.com>
2026-08-08 13:18:28 +0530

    feat: add shadows plugin

---
+++ plugins/shadows/index.ts
@@ -0,0 +1,17 @@
+import {
+    ShadowEnable_,
+    ShadowGenerator_,
+    ShadowDisable_,
+    ShadowRecieveDisable_,
+    ShadowRecieveEnable_
+} from "./private/Core";
+
+const EunoiaEngine_Shadows = {
+    ShadowDisable: ShadowDisable_,
+    ShadowEnable: ShadowEnable_,
+    ShadowGenerator: ShadowGenerator_,
+    ShadowRecieveDisable: ShadowRecieveDisable_,
+    ShadowRecieveEnable: ShadowRecieveEnable_
+};
+
+export default EunoiaEngine_Shadows;
---
+++ plugins/shadows/private/Core.ts
@@ -0,0 +1,30 @@
+import { AbstractMesh, IShadowLight, Scene, ShadowGenerator } from "@babylonjs/core";
+
+(window as any).EunoiaEngine_ShadowGenerators = [];
+
+export async function ShadowGenerator_(light: IShadowLight, resolution: 2 | 4 | 8 | 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096 = 512) {
+    const SG = new ShadowGenerator(resolution, light);
+    SG.id = `${Math.floor(Math.random() * 9999999999) + 1111111111}`;
+    ((window as any).EunoiaEngine_ShadowGenerators as ShadowGenerator[]).push(SG);
+    return SG;
+}
+
+export async function ShadowEnable_(mesh: AbstractMesh) {
+    ((window as any).EunoiaEngine_ShadowGenerators as ShadowGenerator[]).forEach((SG) => {
+        SG.addShadowCaster(mesh);
+    });
+}
+
+export async function ShadowDisable_(mesh: AbstractMesh) {
+    ((window as any).EunoiaEngine_ShadowGenerators as ShadowGenerator[]).forEach((SG) => {
+        SG.removeShadowCaster(mesh);
+    });
+}
+
+export async function ShadowRecieveEnable_(mesh: AbstractMesh) {
+    mesh.receiveShadows = true;
+}
+
+export async function ShadowRecieveDisable_(mesh: AbstractMesh) {
+    mesh.receiveShadows = false;
+}
